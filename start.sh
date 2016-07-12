79a2fd63a06491096402445c5296595b0cc28f07
curcuz <czkzcz.cmc@gmail.com>
2016-07-12 06:24:11 +0200

    be sure those commands get executed

--- start.sh
+++ start.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 #Remove default audio
-rmmod snd_bcm2835 > /dev/null 2>&1 || true
+rmmod snd_bcm2835 || true
 
 # Enable i2c and other interfaces
-modprobe i2c-bcm2708 > /dev/null 2>&1 || true
-modprobe snd-soc-pcm512x > /dev/null 2>&1 || true
-modprobe snd-soc-wm8804 > /dev/null 2>&1 || true
+modprobe i2c-dev || true
+modprobe i2c-bcm2708 || true
+modprobe snd-soc-pcm512x || true
+modprobe snd-soc-wm8804 || true
 
 # Ensure mopidy folders are there
-mkdir /data/mopidy > /dev/null 2>&1 || true
-mkdir /data/mopidy/local > /dev/null 2>&1 || true
-mkdir /data/mopidy/media > /dev/null 2>&1 || true
-mkdir /data/mopidy/playlists > /dev/null 2>&1 || true
+mkdir /data/mopidy || true
+mkdir /data/mopidy/local || true
+mkdir /data/mopidy/media || true
+mkdir /data/mopidy/playlists || true
 
 while true; do
     node /app/index.js
