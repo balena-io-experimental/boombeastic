18088c68679542820572ba453a3fcc593bf82ee5
curcuz <czkzcz.cmc@gmail.com>
2016-07-12 06:57:26 +0200

    back to basics i2c

--- start.sh
+++ start.sh
@@ -5,9 +5,6 @@ rmmod snd_bcm2835 || true
 
 # Enable i2c and other interfaces
 modprobe i2c-dev || true
-modprobe i2c-bcm2708 || true
-modprobe snd-soc-pcm512x || true
-modprobe snd-soc-wm8804 || true
 
 # Ensure mopidy folders are there
 mkdir /data/mopidy || true
