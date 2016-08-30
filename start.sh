5bb42dca36b73692b88e035c41a6f85ac640d80e
curcuz <czkzcz.cmc@gmail.com>
2016-08-30 22:27:44 +0200

    prevent ugly error msg on app update

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #Remove default audio
-rmmod snd_bcm2835 || true
+rmmod snd_bcm2835  >/dev/null 2>&1 || true
 
 # Enable i2c and other interfaces
 modprobe i2c-dev || true
 
 # Ensure mopidy folders are there
-mkdir /data/mopidy || true
-mkdir /data/mopidy/local || true
-mkdir /data/mopidy/media || true
-mkdir /data/mopidy/playlists || true
+mkdir /data/mopidy  >/dev/null 2>&1 || true
+mkdir /data/mopidy/local  >/dev/null 2>&1 || true
+mkdir /data/mopidy/media  >/dev/null 2>&1 || true
+mkdir /data/mopidy/playlists  >/dev/null 2>&1 || true
 
 while true; do
     node /app/index.js
