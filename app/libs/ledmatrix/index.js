c330e864aa1a293eac75524cb87c67fbe4ca39c6
Carlo Maria Curinga <czkzcz.cmc@gmail.com>
2017-02-17 20:47:33 +0100

    support blink

--- app/libs/ledmatrix/index.js
+++ app/libs/ledmatrix/index.js
@@ -110,15 +110,15 @@
             ],
 
             "wifi": [
-               1, 1, 1, 1, 1, 1, 1, 1,
-               0, 0, 0, 0, 0, 0, 0, 0,
-               0, 1, 1, 1, 1, 1, 1, 0,
-               0, 0, 0, 0, 0, 0, 0, 0,
-               0, 0, 1, 1, 1, 1, 0, 0,
-               0, 0, 0, 0, 0, 0, 0, 0,
-               0, 0, 0, 1, 1, 0, 0, 0,
-               0, 0, 0, 1, 1, 0, 0, 0,
-           ],
+                1, 1, 1, 1, 1, 1, 1, 1,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 1, 1, 1, 1, 1, 1, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 1, 1, 1, 1, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 1, 1, 0, 0, 0,
+                0, 0, 0, 1, 1, 0, 0, 0,
+            ],
 
             "blank": [
                 0, 0, 0, 0, 0, 0, 0, 0,
@@ -131,7 +131,7 @@
                 0, 0, 0, 0, 0, 0, 0, 0,
             ]
         };
-
+        this.blinkToggle = 0;
         this.randomImage = [];
 
     };
@@ -143,12 +143,38 @@
     display.prototype.image = function(img) {
         "use strict";
         let self = this;
+        if (self.blinking) {
+            clearInterval(self.blinking);
+            self.image(self.presets.blank);
+        }
         matrix.writeArray(img.reverse());
         img.reverse();
+    };
+    display.prototype.startBlink = function(img, interval) {
+        'use strict';
+        self.blinking = setInterval(() => {
+            self.blinkToggle = !self.blinkToggle;
+            if (self.blinkToggle) {
+                self.image(img);
+            } else {
+                self.image(self.presets.blank);
+            }
+        }, interval);
+    };
+    display.prototype.stopBlink = function(img, interval) {
+        'use strict';
+        if (self.blinking) {
+            clearInterval(self.blinking);
+            self.image(self.presets.blank);
+        }
     };
     display.prototype.random = function() {
         "use strict";
         let self = this;
+        if (self.blinking) {
+            clearInterval(self.blinking);
+            self.image(self.presets.blank);
+        }
         self.randomImage = randomIntArray({
             count: 64,
             max: 2
