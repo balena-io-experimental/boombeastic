74d56d7ee95c2b6d4c7b847e72ef0bcae6c9ba7d
curcuz <czkzcz.cmc@gmail.com>
2017-05-05 22:51:46 +0200

    implement latest changes for resin-wifi-connect

--- app/start.sh
+++ app/start.sh
@@ -16,13 +16,14 @@ mkdir /data/mopidy/cache >/dev/null 2>&1 || true
 
 # Start resin-wifi-connect
 node /usr/src/app/wifi_connect.js || true
+sleep 1 # Delay needed to avoid DBUS introspection errors
 printf "Checking if we are connected to the internet via a google ping...\n\n"
 wget --spider http://google.com 2>&1
 if [ $? -eq 0 ]; then
 	printf "\nconnected to internet, skipping wifi-connect\n\n"
 else
 	printf "\nnot connected, starting wifi-connect\n\n"
-	node src/app.js
+	node src/app.js --clear=false
 fi
 
 # Start haproxy
