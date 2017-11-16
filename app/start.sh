2ff3ebeea3faaeeb5e106d696cb3b6fc56fbfd96
curcuz <czkzcz.cmc@gmail.com>
2017-11-16 18:57:12 +0100

    update wifi connect and remove mopidy

--- app/start.sh
+++ app/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+systemctl start shairport-sync
 
 # Remove default audio
 rmmod snd_bcm2835  >/dev/null 2>&1 || true
@@ -11,15 +12,6 @@ modprobe i2c-dev || true
 # let user know we are loading the app
 node led_loading.js || true
 
-# Ensure mopidy folders are there
-mkdir -p /data/mopidy/local >/dev/null 2>&1 || true
-mkdir /data/mopidy/media >/dev/null 2>&1 || true
-mkdir /data/mopidy/playlists >/dev/null 2>&1 || true
-mkdir /data/mopidy/cache >/dev/null 2>&1 || true
-
-# Ensure mopidy folders are accessible to the mopidy user
-chown -R mopidy:audio /data/mopidy
-
 # resin-wifi-connect
 sleep 1 # Delay needed to avoid DBUS introspection errors
 printf "Checking if we are connected to the internet via a google ping...\n\n"
@@ -29,10 +21,8 @@ if [ $? -eq 0 ]; then
 else
   printf "\nnot connected, starting wifi-connect\n\n"
   node led_wifi_connect.js || true
-  node src/app.js --clear=true
+  ./wifi-connect --clear=true
 fi
 
-# Start haproxy
-service haproxy start >/dev/null 2>&1 || true
-
+# Start app
 node /usr/src/app/index.js
