840f07bd7490238f412f894b01170e7cf6917134
curcuz <czkzcz.cmc@gmail.com>
2018-04-09 03:34:03 +0200

    add support for spotify internet streaming

--- app/start.sh
+++ app/start.sh
@@ -6,7 +6,11 @@ rmmod snd_bcm2835  >/dev/null 2>&1 || true
 
 # Set raspotify to show the device name using resin device name
 sed -i -e "s/BoomBeastic/$RESIN_DEVICE_NAME_AT_INIT/g" /etc/default/raspotify
+if [ "$SPOTIFY_USERNAME" ] && [ "$SPOTIFY_PASSWORD" ]; then
+  sed -i -e "s/#OPTIONS=\"--username <USERNAME> --password <PASSWORD>\"/OPTIONS=\"--username $SPOTIFY_USERNAME --password $SPOTIFY_PASSWORD\"/g" /etc/default/raspotify
+fi
 sed -i -e "s/BoomBeastic/$RESIN_DEVICE_NAME_AT_INIT/g" /usr/local/etc/shairport-sync.conf
+
 # Start cast services
 systemctl start shairport-sync
 systemctl start raspotify
