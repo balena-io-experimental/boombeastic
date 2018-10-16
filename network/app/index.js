9fdd572f9baf58d3c6ed7c8ee8f83c7c13b87f63
curcuz <czkzcz.cmc@gmail.com>
2018-10-16 21:51:04 +0200

    handle wifi-connect from the node process

--- network/app/index.js
+++ network/app/index.js
@@ -2,13 +2,33 @@
 
 {
   const chalk = require("chalk");
+  const isOnline = require('is-online');
   const io = require('socket.io-client');
   const socket = io('http://localhost:1337');
   const debug = require('debug')('main');
-
+  const {
+    spawn
+  } = require('child_process');
+  let wifiConnect;
   socket.on('connect', () => {
     'use strict';
     socket.emit('identify', 'network');
+  });
+
+  isOnline().then(online => {
+    "use strict";
+    if (!online) {
+      console.log(chalk.yellow("online check failed, spawning wifi-connect"));
+      socket.emit("emoji", "wifi");
+      wifiConnect = spawn(__dirname + '/.wifi-connect', ['-a', '600']);
+      wifiConnect.on('close', (code) => {
+        console.log(chalk.cyan("wifi-connect exited"));
+        socket.emit('emoji', "smile");
+      });
+      wifiConnect.on('data', (output) => {
+        console.log(chalk.cyan("wifi-connect: ")+output);
+      });
+    }
   });
 
 }
